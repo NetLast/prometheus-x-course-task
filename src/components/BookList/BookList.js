aa9f27ce03a2525f6517df378e46840bb0b188cd
netla <natlastovets@gmail.com>
2024-07-10 22:33:25 +0300

    initial commit

---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import './BookList.css';
+import defaultImage from '../../images/default-book-image.jpg';
+import { FaSearch } from 'react-icons/fa';
+import { BookContext } from '../../contexts/BookContext';
+import {TbDeviceImacSearch} from "react-icons/tb";
+
+const BookList = () => {
+  const { books, setBooks } = useContext(BookContext);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [filterTitle, setFilterTitle] = useState('');
+  const [filterPrice, setFilterPrice] = useState('');
+
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/books.json`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.books) {
+          setBooks(data.books);
+        } else {
+          throw new Error('Data format is incorrect');
+        }
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, [setBooks]);
+
+  if (!books) {
+    return <div>Loading...</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const filteredBooks = books.filter(book => {
+    return (
+      book.title.toLowerCase().includes(filterTitle.toLowerCase()) &&
+      (filterPrice === '' || book.price <= parseFloat(filterPrice))
+    );
+  });
+
+  return (
+    <section className="container-books">
+      <div id="filters">
+        <div className="filter-group-name">
+          <input id="filters-input"
+                 type="text"
+                 placeholder="Фільтр за назвою"
+                 value={filterTitle}
+                 onChange={(e) => setFilterTitle(e.target.value)}
+          />
+          <div id="fa"><FaSearch></FaSearch></div>
+        </div>
+        <div className="filter-group-price">
+          <input id="filters-input"
+                 type="number"
+                 placeholder="Фільтр за ціною"
+                 value={filterPrice}
+                 onChange={(e) => setFilterPrice(e.target.value)}
+          />
+          <div id="fa"><TbDeviceImacSearch></TbDeviceImacSearch></div>
+        </div>
+      </div>
+      <div className="book-list">
+        {filteredBooks.map(book => (
+          <div key={book.id} className="book-item">
+            <img src={book.image || defaultImage} alt={book.title} className="book-image" />
+            <div className="book-details">
+              <h3>{book.title}</h3>
+              <p>Автор: {book.author}</p>
+            </div>
+            <div className="price-and-button">
+              <p id="book-price-list">{book.price} грн</p>
+              <Link to={`/books/${book.id}`}>
+                <button className="button-show">Переглянути</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default BookList;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import './Header.css';
+import cartIcon from '../../images/cart.svg';
+import avatar from '../../images/avatar.png';
+
+
+const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const username = localStorage.getItem('username');
+  const avatar = localStorage.getItem('avatar');
+
+  const handleSignOut = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('avatar');
+    navigate('/signin');
+  };
+
+  const isSignInPage = location.pathname === '/signin';
+
+  return (
+    <header>
+      <div className="bookstore">
+        <nav>
+          <h3>X-course task /Nataliia Lastovets</h3>
+          {!isSignInPage && (
+            <>
+              <div className="right-section">
+                <Link to="/cart">
+                  <img src={cartIcon} alt="cart" className="cart-icon"/>
+                </Link>
+                {username ? (
+                  <>
+                    <button className="sign-out-btn" onClick={handleSignOut}>Sign-Out</button>
+                    <div className="user-info">
+                        <img src={avatar} alt="User avatar" className="user-avatar" />
+                      <Link to="/books">
+                        <span id="user-header">{username}</span>
+                      </Link>
+                    </div>
+                  </>
+                ) : (
+                  <Link to="/signin">Sign In</Link>
+                )}
+              </div>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
